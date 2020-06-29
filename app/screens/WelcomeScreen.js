e579cef388930004cfc94ed3a570a45ab0314aa8
sahilpatel18 <sahil.41801@gmail.com>
2020-06-29 18:03:33 -0400

    Login/Register page styling + reusable style components

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+import AppButton from "../components/AppButton";
+
+const WelcomeScreen = (props) => {
+  return (
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      blurRadius={10}
+      style={styles.background}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo-red.png")} style={styles.logo} />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title='Login' />
+        <AppButton title='Register' color='secondary' />
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    width: "100%",
+    padding: 20,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: '600',
+    paddingVertical: 20,
+  }
+});
+
+export default WelcomeScreen;
