a3d9ad030e9eca6e82672c0b25c64bc07d89a6ed
sahilpatel18 <sahil.41801@gmail.com>
2020-07-01 14:48:34 -0400

    Text input component and reusable styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { SafeAreaView, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, TextInput } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
@@ -12,7 +12,14 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <AppTextInput placeholder='email' icon='email' />
+    </Screen>
+  );
 }
