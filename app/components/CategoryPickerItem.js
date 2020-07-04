4b91de90c824e1d4ca9e60051a5de5e363b367f2
sahilpatel18 <sahil.41801@gmail.com>
2020-07-04 12:25:50 -0400

    getting users location on list edit page

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,30 +1,34 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Icon from './Icon'
-import AppText from './Text'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Icon from "./Icon";
+import AppText from "./Text";
 
-export default function CategoryPickerItem({onPress, item}) {
-    return (
-        <View style={styles.container}>
-            <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
-            <AppText style={styles.label}>{item.label}</AppText>
-        </View>
-    )
+export default function CategoryPickerItem({ onPress, item }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+      </TouchableOpacity>
+
+      <AppText style={styles.label}>{item.label}</AppText>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container:{
-paddingHorizontal: 30,
-paddingVertical: 15,
-alignItems: 'center',
-width: '33%'
-},
-label:{
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
     marginTop: 5,
-    textAlign:'center',
+    textAlign: "center",
     fontSize: 15,
-
-}
-})
-
-
+  },
+});
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
+import * as Location from 'expo-location'
 
 import {
   AppForm,
@@ -79,6 +80,20 @@ const categories = [
 ];
 
 function ListingEditScreen() {
+
+const [location, setLocation] = useState()
+
+  const getLocation = async () => {
+    const {granted} = await Location.requestPermissionsAsync()
+    if (!granted) return 
+   const {coords: {latitude, longitude}} = await Location.getLastKnownPositionAsync()
+   setLocation({latitude, longitude})
+  }
+
+useEffect(() => {
+getLocation()
+}, [])
+
   return (
     <Screen style={styles.container}>
       <AppForm
@@ -89,7 +104,7 @@ function ListingEditScreen() {
           category: null,
           images: []
         }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => console.log(location)}
         validationSchema={validationSchema}
       >
         <FormImagePicker name='images' />
