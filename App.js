ef5a34c7a48dbc78d4db59aa72029f4310fb9b51
sahilpatel18 <sahil.41801@gmail.com>
2020-07-05 14:16:39 -0400

    bottom tab nav for logged in users

--- App.js
+++ App.js
@@ -1,43 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import {
-  SafeAreaView,
-  View,
-  Text,
-  TextInput,
-  Switch,
-  ImagePickerIOS,
-  Picker,
-  Button,
-  Image,
-} from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import React from "react";
+import { Text, Button } from "react-native";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/lists/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/TextInput";
-import AppPicker from "./app/components/Picker";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import AppText from "./app/components/Text";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthNavigator from "./app/Navigation/AuthNavigator";
+import navigationTheme from "./app/Navigation/navigationTheme";
+import AppNavigator from "./app/Navigation/AppNavigator";
 
 export default function App() {
-  
-  
-
   return (
-    <ListingEditScreen />
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
---
+++ app/Navigation/AppNavigator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ListingsScreen from "../screens/ListingsScreen";
+import ListingEditScreen from "../screens/ListingEditScreen";
+import AccountScreen from "../screens/AccountScreen";
+
+const Tab = createBottomTabNavigator();
+export default AppNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Listings' component={ListingsScreen} />
+      <Tab.Screen name='ListingEdit' component={ListingEditScreen} />
+      <Tab.Screen name='Account' component={AccountScreen} />
+    </Tab.Navigator>
+  );
+};
