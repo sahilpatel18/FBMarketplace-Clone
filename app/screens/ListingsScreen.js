30c6cae160b1553b9f6682c9e809a4470c58a283
sahilpatel18 <sahil.41801@gmail.com>
2020-07-09 15:25:05 -0400

    Retry button if data cannot be retrieved from api

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
+import Button from '../components/Button'
 import Screen from "../components/Screen";
 import routes from "../Navigation/routes";
 import listingsApi from "../api/listings";
 import Card from "../components/Card";
 import colors from "../config/colors";
+import AppText from '../components/Text'
 
 export default function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -15,11 +18,20 @@ export default function ListingsScreen({ navigation }) {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve listings.</AppText>
+          <Button title='Retry' onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
