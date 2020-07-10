777f9f319a31ab18dafe0f2188afcfbbb6081332
sahilpatel18 <sahil.41801@gmail.com>
2020-07-10 14:59:44 -0400

    form reset after submition using formik

--- app/components/forms/FormField.js
+++ app/components/forms/FormField.js
@@ -4,14 +4,21 @@ import { useFormikContext } from "formik";
 import AppTextInput from "../TextInput";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormField({ name,width, ...otherProps }) {
-  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+function AppFormField({ name, width, ...otherProps }) {
+  const {
+    setFieldTouched,
+    setFieldValue,
+    values,
+    errors,
+    touched,
+  } = useFormikContext();
 
   return (
     <>
       <AppTextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={text => setFieldValue(name, text) }
+        value={values[name]}
         width={width}
         {...otherProps}
       />
