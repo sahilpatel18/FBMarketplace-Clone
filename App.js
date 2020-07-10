dd8483eda348021f01de571f840b27ce59810b99
sahilpatel18 <sahil.41801@gmail.com>
2020-07-10 16:09:16 -0400

    implementing cache layer using async-storage and moment

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Text, Button } from "react-native";
-import Screen from "./app/components/Screen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import AuthNavigator from "./app/Navigation/AuthNavigator";
-import navigationTheme from "./app/Navigation/navigationTheme";
-import AppNavigator from "./app/Navigation/AppNavigator";
+import { AsyncStorage } from "@react-native-community/async-storage";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  demo();
+  return null;
 }
